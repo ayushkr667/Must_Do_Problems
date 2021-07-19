0410620dfd8f9bfe036c73407659e5e04e23dda3
Ayush kumar <ayushkr667@gmail.com>
2021-07-20 01:23:02 +0530

    Create  Implement strStr().cpp

---
+++ Day15 (String)/Implement strStr().cpp
@@ -0,0 +1,40 @@
+// Implement strStr().
+
+// Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+// Clarification:
+
+// What should we return when needle is an empty string? This is a great question to ask during an interview.
+
+// For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
+
+// Example 1:
+
+// Input: haystack = "hello", needle = "ll"
+// Output: 2
+// Example 2:
+
+// Input: haystack = "aaaaa", needle = "bba"
+// Output: -1
+
+//O(m*n)
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int m = haystack.size(), n = needle.size();
+        for (int i = 0; i <= m - n; i++) {
+            int j = 0;
+            for (; j < n; j++) {
+                if (haystack[i + j] != needle[j]) {
+                    break;
+                }
+            }
+            if (j == n) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
+
+//ANOTHER solution is KMP search which has O(m+n)
