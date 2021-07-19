40466852fd6a0942fae7289e7194b883d1057928
Ayush kumar <ayushkr667@gmail.com>
2021-07-20 00:31:26 +0530

    Create Roman to Integer.cpp

---
+++ Day15 (String)/Roman to Integer.cpp
@@ -0,0 +1,52 @@
+// Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+// Symbol       Value
+// I             1
+// V             5
+// X             10
+// L             50
+// C             100
+// D             500
+// M             1000
+// For example, 2 is written as II in Roman numeral, just two one's added together. 
+// 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+// Roman numerals are usually written largest to smallest from left to right. However, 
+// the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before 
+// the five we subtract it making four. The same principle applies to the number nine, 
+// which is written as IX. There are six instances where subtraction is used:
+
+// I can be placed before V (5) and X (10) to make 4 and 9. 
+// X can be placed before L (50) and C (100) to make 40 and 90. 
+// C can be placed before D (500) and M (1000) to make 400 and 900.
+// Given a roman numeral, convert it to an integer.
+
+// Example 1:
+
+// Input: s = "III"
+// Output: 3
+// Example 2:
+
+// Input: s = "IV"
+// Output: 4
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> symbols;
+        symbols['I'] = 1;   symbols['V'] = 5;   symbols['X'] = 10;  
+        symbols['L'] = 50;  symbols['C'] = 100; symbols['D'] = 500; 
+        symbols['M'] = 1000;
+        
+        int number = 0;
+        for(int i =0; i<s.size(); i++){
+            number += symbols[s[i]];
+            int j = i-1;
+            if(j >= 0 && (symbols[s[j]] < symbols[s[i]])){
+                number -= (2*(symbols[s[j]]));
+            }
+        }
+        
+        return number;
+    }
+};
