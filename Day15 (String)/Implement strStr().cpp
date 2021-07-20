bf209205b4a406af2f7df74be610d19adeaa7119
Ayush kumar <ayushkr667@gmail.com>
2021-07-20 17:04:52 +0530

    Update Implement strStr().cpp

--- Day15 (String)/Implement strStr().cpp
+++ Day15 (String)/Implement strStr().cpp
@@ -1,45 +1,74 @@
-// Implement strStr().
+// O(m*n)
+// class Solution {
+// public:
+//     int strStr(string haystack, string needle) {
+//         int m = haystack.size(), n = needle.size();
+//         for (int i = 0; i <= m - n; i++) {
+//             int j = 0;
+//             for (; j < n; j++) {
+//                 if (haystack[i + j] != needle[j]) {
+//                     break;
+//                 }
+//             }
+//             if (j == n) {
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+// };
 
-// Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
 
-// Clarification:
-
-// What should we return when needle is an empty string? This is a great question to ask during an interview.
-
-// For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
-
-// Example 1:
-
-// Input: haystack = "hello", needle = "ll"
-// Output: 2
-// Example 2:
-
-// Input: haystack = "aaaaa", needle = "bba"
-// Output: -1
-
-//O(m*n)
+// //we can solve this(find a string inside other string) by using Rabin karp algorithm also
+//     //-> basic idea of rabin carp is to use hash code to avoid checking again
+//     //-> and again. O(n-m+1) is best case still worset case is O(m*n)
+// 99991 is prime number , modding to get rid of overflow
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        int m = haystack.size(), n = needle.size();
-        for (int i = 0; i <= m - n; i++) {
-            int j = 0;
-            for (; j < n; j++) {
-                if (haystack[i + j] != needle[j]) {
-                    break;
+        if(!needle.size())
+            return 0;
+        long long hashFn = 0;
+        long long sub = 1;
+        
+        //calculating hash function 
+        for(int i=0; i<needle.size(); i++){
+            long long temp = (needle[i] - 'a' + 1);
+            hashFn = ((hashFn*10)%(99991) + temp)%(99991);
+            if(i > 0)
+                sub = (sub * 10)%(99991);
+        }
+        
+        long long value = 0;
+        int st = 0;
+        for(int i=0; i<haystack.size(); i++){
+            long long temp = (haystack[i] - 'a' + 1);
+            value = ((value*10)%(INT_MAX - 1) + temp)%(99991);
+            if(value < 0)
+                value += 99991;
+            
+            if(i >= needle.size() -1){
+                //then compare each character of string
+                if(value == hashFn){
+                    bool flag = true;
+                    int l, l1;
+                    for(l = st, l1=0; l < haystack.size() && l1 < needle.size(); l++, l1++){
+                        if(haystack[l] != needle[l1]){
+                            flag = false;
+                            break;
+                        }
+                    }
+                    
+                    if(flag && l1 == needle.size())
+                        return st;
                 }
-            }
-            if (j == n) {
-                return i;
+                value -= ((haystack[st] - 'a' + 1) * sub);
+                st++;
             }
         }
         return -1;
     }
 };
-//we can solve this by using Rabin karp algorithm also
-    //-> basic idea of rabin carp is to use hash code to avoid checking again
-    //-> and again. O(n-m+1) is best case still worset case is O(m*n)
-
 
 
 
