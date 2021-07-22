87c6c4afe08ffaecfce8f6d919d2a8f5bdcbd893
Ayush kumar <ayushkr667@gmail.com>
2021-07-22 17:49:43 +0530

    Update Top View of Binary Tree.cpp

--- Day17 (Binary Tree)/Top View of Binary Tree.cpp
+++ Day17 (Binary Tree)/Top View of Binary Tree.cpp
@@ -26,26 +26,38 @@
 // 40   60  90    100
 // Output: 40 20 10 30 100
 
+// O(n)
+
 class Solution
 {
     public:
     vector<int> topView(Node *root)
     {
-        map<int, int> mp;
         vector<int> res;
         if(!root)
             return res;
             
         queue<pair<Node*, int>> Q;
         Q.push({root, 0});
+        
+        int max_left  = 0, max_right = 0;
+        vector<int> lft, rit;
+        rit.push_back(root->data);
+        
         while(!Q.empty()){
             int n = Q.size();
             while(n--){
                 auto t = Q.front();  Q.pop();
                 auto temp = t.first;
                 auto key = t.second;
-                if(mp.find(key) == mp.end())
-                    mp[key] = temp->data;
+                if(key < max_left){
+                    max_left = key;
+                    lft.push_back(temp->data);
+                }
+                if(key > max_right){
+                    max_right = key;
+                    rit.push_back(temp->data);
+                }
                     
                 if(temp->left)
                     Q.push({temp->left, key-1});
@@ -53,10 +65,48 @@ class Solution
                     Q.push({temp->right, key+1});
             }
         }
-        for(auto it = mp.begin(); it!=mp.end(); it++){
-            res.push_back(it->second);       
+        for(int i=lft.size()-1; i>=0; i--){
+            res.push_back(lft[i]);
+        }
+        for(auto i: rit){
+            res.push_back(i);
         }
         return res;
     }
 
 };
+
+
+// O(nlog n)
+// class Solution
+// {
+//     public:
+//     vector<int> topView(Node *root)
+//     {
+//         map<int, int> mp;
+//         vector<int> res;
+//         if(!root)
+//             return res;
+            
+//         queue<pair<Node*, int>> Q;
+//         Q.push({root, 0});
+//         while(!Q.empty()){
+//             int n = Q.size();
+//             while(n--){
+//                 auto t = Q.front();  Q.pop();
+//                 auto temp = t.first;
+//                 auto key = t.second;
+//                 if(mp.find(key) == mp.end())
+//                     mp[key] = temp->data;                   
+//                 if(temp->left)
+//                     Q.push({temp->left, key-1});
+//                 if(temp->right)
+//                     Q.push({temp->right, key+1});
+//             }
+//         }
+//         for(auto it = mp.begin(); it!=mp.end(); it++){
+//             res.push_back(it->second);       
+//         }
+//         return res;
+//     }
+// };
