132d8eb067bfc925e298d2798b5f58a19a270c04
Ayush kumar <ayushkr667@gmail.com>
2021-07-23 01:16:30 +0530

    Create Binary Tree Zigzag Level Order Traversal.cpp

---
+++ Day18 (Binary Tree)/Binary Tree Zigzag Level Order Traversal.cpp
@@ -0,0 +1,62 @@
+// Given the root of a binary tree, return the zigzag level order traversal 
+// of its nodes' values. (i.e., from left to right, then right to left for the next level and alternate between).
+
+// Example 1:
+
+
+// Input: root = [3,9,20,null,null,15,7]
+// Output: [[3],[20,9],[15,7]]
+// Example 2:
+
+// Input: root = [1]
+// Output: [[1]]
+
+//spiral order traversal
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        if (!root) return {};
+        
+        queue<TreeNode*> q;
+        vector<vector<int>> out;
+            
+        q.push(root);  
+        int level = 0;
+        
+        while (!q.empty()) {
+            
+            int sz = q.size();  
+            vector<int> curr(sz); 
+            
+            for (int i = 0; i < sz; i++) {
+                
+                TreeNode* tmp = q.front();
+                q.pop();
+                
+                if (level == 0) {
+                    curr[i] = tmp->val;
+                } else {
+                    curr[sz - i - 1] = tmp->val; 
+                }
+                
+                if (tmp->left) q.push(tmp->left);
+                if (tmp->right) q.push(tmp->right);
+            }
+            out.push_back(curr); 
+            level = !level; 
+        }
+        return out;
+    }
+};
